f9b64fff09763d23cf280aa5091963aa79aa143a
Ersella <101304385+Ersella@users.noreply.github.com>
2022-09-10 17:09:10 +0700

    tugas4_dpw2

---
+++ System/resources/views/template/section/sidebar.blade.php
@@ -0,0 +1,89 @@
+@php
+    function checkRouteActive($route){
+        if(Route::current()->uri == $route) return 'active';
+    }
+@endphp
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img height="50px" src="{{url('public')}}/images/logo.png" alt="" class="" style="">
+      <span class="brand-text font-weight-light"></span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{url('public')}}/images/saya.png" class="img-circle elevation-3" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block-center">FebrianiErsella</a>
+        </div>
+      </div>
+      <!-- SidebarSearch Form -->
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="/beranda" class="nav-link {{checkRouteActive('beranda')}}">
+              <i class="nav-icon fas fa-home"></i>
+              <p>
+                Beranda
+              </p>
+            </a>
+          </li> 
+          <li class="nav-item">
+            <a href="/produk" class="nav-link {{checkRouteActive('produk')}}">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Produk
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/kategori" class="nav-link {{checkRouteActive('kategori')}}">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Kategori
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/promo" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Promo
+              </p>
+            </a>
+          </li>
+        <li class="nav-item menu-open">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Master Data
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Pelanggan</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Supplier</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
